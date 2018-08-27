7a6a5e27b0f03031f46912e769196f3134188c17
mytpp <yourtpp@outlook.com>
2018-08-27 17:49:54 +0800

    add comments

--- NDimensionArray/NDimArray.h
+++ NDimensionArray/NDimArray.h
@@ -15,6 +15,9 @@ A implementation for n-dimension-array
 
 namespace mytpp {
 
+	//T: type of elements in the NDimArray
+	//dim_size: size of current dimension
+	//args: sizes of following dimensions
 	template <typename T, size_t dim_size, size_t... args>
 	class NDimArray
 	{
@@ -34,60 +37,70 @@ namespace mytpp {
 		NDimArray& operator=(NDimArray&&) = default;
 
 
+		//Return the dimension of this array
 		size_t dim() {
 			return sizeof...(args) + 1;
 		}
 
+		//Return # of elements in dimension 'dim'.
+		//Note that 'dim' starts from 0.
 		size_t size_of_dim(size_t dim) {
 			assert(dim <= sizeof...(args) + 1);
 			size_t tmp[] = { dim_size, args... };
 			return tmp[dim];
 		}
 
+		//Return the total # of elements in this array
 		size_t size() {
 			return value.size();
 		}
 
 
-		//overload operator[] to allow for this kind of syntax:
+		//Overload operator[] to allow for this kind of syntax:
 		//NDimArray<int, 50, 100> a;  a[17][18] = 2333;
+		//Return a proxy class which also overloads operator[]
+		//The true element is returned at the partial specialization version
 		NDimArray<T, args...>&  operator[] (size_t index) {
 			assert(index < dim_size);
 			value.setOffset(m_offset*dim_size + index);
 			return value;
 		}
 
-		//this is only for implementation
+		//This is only for implementation
 		void setOffset(size_t offset) {
 			m_offset = offset;
 		}
 
 
 	private:
-		size_t m_offset;
-		NDimArray<T, args...> value;
+		size_t m_offset;             //current offset
+		NDimArray<T, args...> value; //proxy class
 	};
 
 	/*-------------------------------------------------------------------*/
 
-	//partial specialization to end recursion and allocate memory
+	//Partial specialization of one dimension array to end recursion.
+	//And this is the only place where we use 'new expression'.
+	//Hence, we can ensure allocating memory only once when creating a NDimArray.
 	template <typename T, size_t dim_size>
 	class NDimArray<T, dim_size>
 	{
 	public:
+		//default constructor
 		NDimArray()
 			: m_offset(0)
 			, m_size(dim_size)
 			, value(new T[dim_size])
 		{ }
 
+		//This is only for implementation
 		NDimArray(size_t offset)
 			: m_offset(offset)
 			, m_size(dim_size*offset)
 			, value(new T[dim_size*offset])
 		{ }
 
-		//copy constructor and copy assignment operator
+		//copy constructor
 		NDimArray(const NDimArray& rhs)
 			:m_offset(rhs.m_offset)
 			,m_size(rhs.m_size)
@@ -98,7 +111,9 @@ namespace mytpp {
 			do_copy(value, rhs.value, rhs.m_size);
 		}
 
+		//copy assignment operator
 		NDimArray& operator=(const NDimArray& rhs) {
+			//Offer strong exception safety guarantee
 			T* old_memery = value;
 			value = static_cast<T*>(
 				::operator new(sizeof(T)*rhs.m_size));
@@ -117,7 +132,8 @@ namespace mytpp {
 			,m_size(rhs.m_size)
 			,value(rhs.value) 
 		{ 
-			rhs.value = nullptr; 
+			//make 'rhs' destructible
+			rhs.value = nullptr;     
 		}
 
 		//move assignment operator
@@ -126,9 +142,16 @@ namespace mytpp {
 				m_offset = rhs.m_offset;
 				m_size = rhs.m_size;
 				value = rhs.value;
-				rhs.value = nullptr;
+
+				//make 'rhs' destructible
+				rhs.value = nullptr; 
 			}
 			return *this;
+		}
+
+		//destructor
+		~NDimArray() {
+			delete[] value;
 		}
 
 
@@ -145,32 +168,31 @@ namespace mytpp {
 			return m_size;
 		}
 
-		~NDimArray() {
-			delete[] value;
-		}
 
-
+		//Return a real elements of type 'T' at the calculated position
 		T& operator[] (size_t index) {
 			assert(index < dim_size);
 			return value[m_offset*dim_size + index];
 		}
 
-		//this is only for implementation
+		//This is only for implementation
 		void setOffset(size_t offset) {
 			m_offset = offset;
 		}
 
 	private:
+		//Copy a block of memory from 'src' to 'dest'
+		//There are 'length' elements of type 'T' in the memory block
 		void do_copy(T* dest, T* src, size_t length) {
 			for (size_t i = 0; i < length; i++, dest++, src++)
 				::new (static_cast<void*>(dest)) T(*src);
 		}
 
 	private:
-		size_t m_offset;
-		size_t m_size;
+		size_t m_offset;// the position of element returned by operator[]
+		size_t m_size;  // # of elements in the array
 
-		T* value;
+		T* value;       // the memery block storing elements of type 'T'
 	};
 }
 
--- NDimensionArray/main.cpp
+++ NDimensionArray/main.cpp
@@ -5,65 +5,80 @@ Update: 2018-8
 A simple test and example uses for template NDimArray
 **************************************************************************/
 
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+//#define new  new(_CLIENT_BLOCK, __FILE__, __LINE__)
+#endif
+
 #include "NDimArray.h"
 #include <utility>
 
 using namespace std;
 using namespace mytpp;
 
-///////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-	NDimArray<int, 3, 4> a;
-	//NDimArray<int, 1.5> b;//compile error: argument of type "double" is incompatible 
-	                        //with template parameter of type "size_t"
-	//NDimArray<int> c;	    //compile error: too few arguments for class template "NDimArray"
-	NDimArray<double, 2, 3, 4> d;
-	NDimArray<bool, 2, 2, 2, 2, 2, 2> e;
-	NDimArray<long long, 100, 100> bigArray;
-	NDimArray<int, 3, 4> copyImage(a);                  //test copy constructor
-	NDimArray<double, 2, 3, 4> moveImage(std::move(d)); //test move constructor
-	
-	//test for member function NDimArray<..>::dim()
-	assert(a.dim() == 2);
-	assert(d.dim() == 3);
-	assert(e.dim() == 6);
+	//start a scope for testing memery leak
+	{
+		NDimArray<int, 3, 4> a;
+		//NDimArray<int, 1.5> b;//compile error: argument of type "double" is incompatible 
+								//with template parameter of type "size_t"
+		//NDimArray<int> c;	    //compile error: too few arguments for class template "NDimArray"
+		NDimArray<double, 2, 3, 4> d;
+		NDimArray<bool, 2, 2, 2, 2, 2, 2> e;
+		NDimArray<long long, 100, 100> bigArray;
+		NDimArray<int, 3, 4> copyImage(a);                  //test copy constructor
+		NDimArray<double, 2, 3, 4> moveImage(std::move(d)); //test move constructor
 
-	//test for member function NDimArray<..>::size_of_dim(size_t)
-	assert(d.size_of_dim(0) == 2);
-	assert(d.size_of_dim(1) == 3);
-	assert(d.size_of_dim(2) == 4);
+		//test for member function NDimArray<..>::dim()
+		assert(a.dim() == 2);
+		assert(d.dim() == 3);
+		assert(e.dim() == 6);
 
-	//test for member function NDimArray<..>::size()
-	assert(a.size() == 12);
-	assert(d.size() == 24);
-	assert(e.size() == 64);
-	assert(bigArray.size() == 10000);
+		//test for member function NDimArray<..>::size_of_dim(size_t)
+		assert(d.size_of_dim(0) == 2);
+		assert(d.size_of_dim(1) == 3);
+		assert(d.size_of_dim(2) == 4);
+
+		//test for member function NDimArray<..>::size()
+		assert(a.size() == 12);
+		assert(d.size() == 24);
+		assert(e.size() == 64);
+		assert(bigArray.size() == 10000);
 
 
-	//test for NDimArray<..>::operator[]
-	//assgin some initial value to the array
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 4; j++) {
-			a[i][j] = i + j;
-			moveImage[0][i][j] = static_cast<double>(i + j) / 2;
-			moveImage[1][i][j] = static_cast<double>(i + j) / 3;
-		}
+		//test for NDimArray<..>::operator[]
+		//assgin some initial value to the array
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 4; j++) {
+				a[i][j] = i + j;
+				moveImage[0][i][j] = static_cast<double>(i + j) / 2;
+				moveImage[1][i][j] = static_cast<double>(i + j) / 3;
+			}
 
-	//test copy assignment operator
-	copyImage = a;  
+		//test copy assignment operator
+		copyImage = a;
 
-	//test move assignment operator
-	d = std::move(moveImage);
+		//test move assignment operator
+		d = std::move(moveImage);
 
-	//test for correctness
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 4; j++) {
-			assert(copyImage[i][j] == i + j);
-			assert(d[0][i][j] == static_cast<double>(i + j) / 2);
-			assert(d[1][i][j] == static_cast<double>(i + j) / 3);
-		}
+		//test for correctness
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 4; j++) {
+				assert(copyImage[i][j] == i + j);
+				assert(d[0][i][j] == static_cast<double>(i + j) / 2);
+				assert(d[1][i][j] == static_cast<double>(i + j) / 3);
+			}
+	}//end the scope
 
+#ifdef _MSC_VER
+	_CrtDumpMemoryLeaks();
+#endif
+
+	system("pause");
 	return 0;
 }
